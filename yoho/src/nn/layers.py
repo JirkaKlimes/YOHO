e7639f6dd841db15c066ade8a9429e2a89fd7f74
JirkaKlimes <jiriklimes920@gmail.com>
2024-05-17 18:56:09 +0200

    position encoding to each head separately

--- yoho/src/nn/layers.py
+++ yoho/src/nn/layers.py
@@ -23,13 +23,12 @@ class RoPE(nn.Module):
         seq_len, dim = x.shape[-2:]
         assert not dim & 1, "number of input dims mut be even"
 
-        numerator = jnp.arange(0, dim, 2)
-        thetas = 1.0 / (10000 ** (numerator / dim))
+        thetas = 10 ** -jnp.arange(0, dim // 2, dtype=jnp.float32)
         angles = jnp.outer(jnp.arange(1, seq_len + 1), thetas)
         angles = jnp.e ** (1j * angles)
 
         x_complex = x[..., ::2] + 1j * x[..., 1::2]
-        x_rotated = x_complex * angles[None]
+        x_rotated = x_complex * angles
 
         x_out = jnp.empty_like(x)
         x_out = x_out.at[..., ::2].set(x_rotated.real)
@@ -44,7 +43,13 @@ class GroupedQueryAttention(nn.Module):
     kv_heads: Optional[int] = None
 
     @nn.compact
-    def __call__(self, q: jnp.ndarray, kv: Optional[jnp.ndarray] = None):
+    def __call__(
+        self,
+        q: jnp.ndarray,
+        kv: Optional[jnp.ndarray] = None,
+        mask: Optional[jnp.ndarray] = None,
+    ):
+        # TODO: add kv cache
         batch_size, seq_len, in_dims = q.shape
 
         q_heads = self.heads
@@ -54,21 +59,25 @@ class GroupedQueryAttention(nn.Module):
         assert q_heads % kv_heads == 0, "num of kv heads must be divisible by num of q heads"
         assert q_heads >= kv_heads, "num of q heads must be greater or equal to num of kv heads"
 
-        # TODO: check if it's better to apply RoPE to each head separately
-        xq = RoPE()(nn.Dense(q_heads * self.dims, use_bias=False)(q))
-        xk = RoPE()(nn.Dense(kv_heads * self.dims, use_bias=False)(kv))
+        xq = nn.Dense(q_heads * self.dims, use_bias=False)(q)
+        xk = nn.Dense(kv_heads * self.dims, use_bias=False)(kv)
         xv = nn.Dense(kv_heads * self.dims, use_bias=False)(kv)
         xk = xk.repeat(2, axis=-1)
         xv = xv.repeat(2, axis=-1)
 
-        xq = xq.reshape((batch_size, seq_len, q_heads, self.dims))
-        xk = xk.reshape((batch_size, seq_len, q_heads, self.dims))
-        xv = xv.reshape((batch_size, seq_len, q_heads, self.dims))
+        xq = xq.reshape(batch_size, seq_len, q_heads, self.dims)
+        xk = xk.reshape(batch_size, seq_len, q_heads, self.dims)
+        xv = xv.reshape(batch_size, seq_len, q_heads, self.dims)
         xq = xq.transpose(0, 2, 1, 3)
-        xk = xk.transpose(0, 2, 3, 1)
+        xk = xk.transpose(0, 2, 1, 3)
         xv = xv.transpose(0, 2, 1, 3)
 
+        xq = RoPE()(xq)
+        xk = RoPE()(xk)
+
+        xk = xk.transpose(0, 1, 3, 2)
         attention_scores = (xq @ xk) * (self.dims / q_heads) ** -0.5
+        attention_scores -= 1 / mask - 1
         attention_scores = nn.softmax(attention_scores)
         out = (attention_scores @ xv).transpose(0, 2, 1, 3).reshape(batch_size, seq_len, -1)
         out = nn.Dense(in_dims, use_bias=False)(out)
