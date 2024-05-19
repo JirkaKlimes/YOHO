3fa48d72abd586e53ff2b152836cb1e65486e66f
JirkaKlimes <jiriklimes920@gmail.com>
2024-05-19 20:54:06 +0200

    use jax and mp3

--- yoho/src/preprocessing/audio.py
+++ yoho/src/preprocessing/audio.py
@@ -1,8 +1,8 @@
 import ffmpeg
 from pathlib import Path
+import jax
+import jax.numpy as jnp
 import numpy as np
-import scipy
-import scipy.signal
 
 from yoho.src.preprocessing.mel_spectogram import generate_mel_filters
 
@@ -20,7 +20,7 @@ def load_audio(path: Path, sample_rate: int):
 def save_audio(audio: np.ndarray, path: Path, sample_rate: int):
     process = (
         ffmpeg.input("pipe:0", format="s16le", acodec="pcm_s16le", ac=1, ar=sample_rate)
-        .output(str(path.with_suffix(".opus")), format="opus", audio_bitrate="16k")
+        .output(str(path.with_suffix(".mp3")), format="mp3", audio_bitrate="16k")
         .run_async(pipe_stdin=True, quiet=True)
     )
     process.stdin.write(audio.tobytes())
@@ -31,15 +31,15 @@ def save_audio(audio: np.ndarray, path: Path, sample_rate: int):
 def mel_spectogram(
     audio, n_fft: int, hop_len: int, sample_rate: int, n_mels: int, htk: bool = False
 ):
-    stft = scipy.signal.stft(audio, nperseg=n_fft, noverlap=n_fft - hop_len, boundary=None)[-1]
-    magnitudes = np.abs(stft) ** 2
+    stft = jax.scipy.signal.stft(audio, nperseg=n_fft, noverlap=n_fft - hop_len, boundary=None)[-1]
+    magnitudes = jnp.abs(stft) ** 2
     filters = generate_mel_filters(sample_rate, n_fft, n_mels, htk)
     spectogram = filters @ magnitudes
     return spectogram.T
 
 
 def normalize_spectogram(spectogram):
-    normalized = np.log10(np.clip(spectogram, a_min=1e-10))
-    normalized = np.maximum(normalized, normalized.max() - 8.0)
+    normalized = jnp.log10(jnp.clip(spectogram, a_min=1e-10))
+    normalized = jnp.maximum(normalized, normalized.max() - 8.0)
     normalized = (normalized + 4.0) / 4.0
     return normalized
