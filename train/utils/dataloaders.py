25c34e24b6a2126c5dbf43b35da65161871dd9cd
JirkaKlimes <jiriklimes920@gmail.com>
2024-05-19 15:39:43 +0200

    pretraining dataloader

--- train/utils/dataloaders.py
+++ train/utils/dataloaders.py
@@ -3,10 +3,14 @@ import srt
 from eld import LanguageDetector
 import numpy as np
 import bisect
+import datetime as dt
 
 from yoho.src.config import YOHOConfig
+from yoho.src.preprocessing.audio import load_audio, mel_spectogram
+from yoho.src.preprocessing.tokenizer import BPEasyTokenizer
 
 from train.utils.base_dataloader import Dataloader
+from train.utils.standardize_text import standardize
 from train.utils.config import CONFIG
 
 
@@ -14,6 +18,7 @@ class TranscriptionDataloader(Dataloader):
     def __init__(
         self,
         yoho_config: YOHOConfig,
+        tokenizer: BPEasyTokenizer,
         batch_size: int,
         shuffle: bool = True,
         max_queued_batches: int = 8,
@@ -22,13 +27,16 @@ class TranscriptionDataloader(Dataloader):
         use_multiprocessing: bool = True,
     ):
         self.yoho_config = yoho_config
+        self.tokenizer = tokenizer
         self.shuffle = shuffle
 
         language_detector = LanguageDetector()
 
-        all_paths = CONFIG.dataset.noisy.joinpath("transcripts").iterdir()
+        self.root_path = CONFIG.dataset.noisy
+        all_paths = self.root_path.joinpath("transcripts").iterdir()
         sizes = []
         paths = []
+        langs = []
         for path in all_paths:
             with open(path, encoding="utf-8") as f:
                 data = f.read()
@@ -38,10 +46,12 @@ class TranscriptionDataloader(Dataloader):
             if lang not in CONFIG.language_whitelist:
                 continue
             sizes.append(len(transcript))
-            paths.append(path)
+            paths.append((path, self.root_path.joinpath("audio", path.with_suffix(".opus").name)))
+            langs.append(lang)
 
         self.sizes = np.array(sizes, dtype=np.uint64).cumsum()
         self.paths = paths
+        self.langs = langs
         self.index_table = np.arange(self.sizes[-1], dtype=np.uint64)
         if self.shuffle:
             np.random.shuffle(self.index_table)
@@ -50,6 +60,65 @@ class TranscriptionDataloader(Dataloader):
             batch_size, max_queued_batches, num_workers, warmup_queue, use_multiprocessing
         )
 
+    def randomize_padding(self, start_time, end_time, start_speech_time, end_speech_time):
+        duration = (end_speech_time - start_speech_time).total_seconds()
+        padding_left = (start_speech_time - start_time).total_seconds()
+        padding_left = np.random.uniform(
+            0, min(padding_left, self.yoho_config.max_input_seconds - duration)
+        )
+        start_time = start_speech_time - dt.timedelta(seconds=padding_left)
+        duration = (end_speech_time - start_time).total_seconds()
+        padding_right = (end_time - end_speech_time).total_seconds()
+        padding_right = np.random.uniform(
+            0, min(padding_right, self.yoho_config.max_input_seconds - duration)
+        )
+        end_time = end_speech_time + dt.timedelta(seconds=padding_right)
+        return start_time, end_time
+
+    def load_sample_utterances(self, sample_index, transcript, audio, lang):
+        si = sample_index
+
+        utterances = []
+        utterances.append(transcript[si])
+        start_time = dt.timedelta() if si == 0 else transcript[si - 1].end
+        start_speech_time = transcript[si].start
+        while (
+            transcript[si + 1].end - start_speech_time
+        ).total_seconds() < self.yoho_config.max_input_seconds:
+            si += 1
+            utterances.append(transcript[si])
+        end_speech_time = transcript[si].end
+        end_time = (
+            dt.timedelta(seconds=len(audio) / self.yoho_config.sample_rate)
+            if si == len(transcript) - 2
+            else transcript[si + 1].start
+        )
+
+        start_time, end_time = self.randomize_padding(
+            start_time, end_time, start_speech_time, end_speech_time
+        )
+        from_sample = int(np.ceil(start_time.total_seconds() * self.yoho_config.sample_rate))
+        to_sample = int(np.floor(end_time.total_seconds() * self.yoho_config.sample_rate))
+        audio = audio[from_sample:to_sample]
+
+        if len(audio) > self.yoho_config.n_samples:
+            return None, None
+
+        audio = np.pad(audio, (0, self.yoho_config.n_samples - len(audio)))
+
+        utterances_relative = [
+            (
+                int(
+                    np.floor((ut.start - start_time).total_seconds() * self.yoho_config.sample_rate)
+                ),
+                int(np.ceil((ut.end - start_time).total_seconds() * self.yoho_config.sample_rate)),
+                standardize(ut.content, lang=lang),
+            )
+            for ut in utterances
+        ]
+
+        return audio, utterances_relative
+
     def get_num_batches(self) -> int:
         return self.sizes[-1] // self.batch_size
 
@@ -57,33 +126,92 @@ class TranscriptionDataloader(Dataloader):
         i = idx * self.batch_size
         j = (idx + 1) * self.batch_size
 
-        sample_indices = self.index_table[i:j]
-        path_indices = [bisect.bisect_right(self.sizes, i) for i in sample_indices]
+        samples = []
+        for sample_idx in self.index_table[i:j]:
+            while True:
+                asset_index = bisect.bisect_right(self.sizes, sample_idx)
+                transcript_path, audio_path = self.paths[asset_index]
+                lang = self.langs[asset_index]
 
-        transcripts = {}
-        for i in set(path_indices):
-            with open(self.paths[i], encoding="utf-8") as f:
-                data = f.read()
-            transcripts[i] = list(srt.parse(data))
+                with open(transcript_path, encoding="utf-8") as f:
+                    data = f.read()
+                transcript = list(srt.parse(data))
+                audio = load_audio(audio_path, self.yoho_config.sample_rate)
 
-        for si, pi in zip(sample_indices, path_indices):
-            si = int(si - (0 if pi == 0 else self.sizes[pi - 1]))
-            transcript = transcripts[pi]
-            start_time = None if si == 0 else transcript[si - 1].end
-            start_speech_time = transcript[si].start
-            while (
-                transcript[si + 1].end - start_speech_time
-            ).total_seconds() < self.yoho_config.max_input_seconds:
-                si += 1
-            end_speech_time = transcript[si].end
-            end_time = None if si == len(transcript) - 2 else transcript[si + 1].start
+                relative_sample_idx = int(
+                    sample_idx - (0 if asset_index == 0 else self.sizes[asset_index - 1])
+                )
+                audio, utterances = self.load_sample_utterances(
+                    relative_sample_idx, transcript, audio, lang
+                )
+                if audio is not None:
+                    break
 
-            print(f"--> {self.paths[pi].name}")
-            print(start_time)
-            print(start_speech_time)
-            print(end_speech_time)
-            print(end_time)
+                # TODO: this is ugly (if sample is too long, we discard and take next one)
+                sample_idx = int((sample_idx + 1) % self.sizes[-1])
+            samples.append((audio, utterances))
+
+        spectograms_batch = []
+        tokens_batch = []
+
+        for audio, utterances in samples:
+            mel_spec = mel_spectogram(
+                audio,
+                self.yoho_config.n_fft,
+                self.yoho_config.stft_hop,
+                self.yoho_config.sample_rate,
+                self.yoho_config.n_mel_bands,
+                htk=True,
+            )
+            transcript = "<|startoftranscript|>"
+            for start, end, content in utterances:
+                start = int(
+                    min(
+                        np.floor(start / self.yoho_config.stft_hop),
+                        self.yoho_config.max_audio_len - 1,
+                    )
+                )
+                end = int(
+                    min(
+                        np.floor(end / self.yoho_config.stft_hop),
+                        self.yoho_config.max_audio_len - 1,
+                    )
+                )
+                content = f"<|t-{start}|>{content}<|t-{end}|><|voiceprint|>"
+                transcript += content
+            transcript += "<|endoftranscript|>"
+            tokens = self.tokenizer.encode(transcript, allowed_special="all")
+            spectograms_batch.append(mel_spec)
+            tokens_batch.append(tokens)
+
+        spectograms_batch = np.array(spectograms_batch, dtype=np.float32)
+        seq_lengths = np.array([len(s) for s in tokens_batch])
+        tokens_batch = np.array(
+            [np.pad(t, (0, self.yoho_config.max_text_len - len(t))) for t in tokens_batch],
+            dtype=np.uint64,
+        )
+        return spectograms_batch, tokens_batch, seq_lengths
 
 
-dataloader = TranscriptionDataloader(YOHOConfig(), 16, use_multiprocessing=False, shuffle=False)
-dataloader.get_prepared_batch()
+if __name__ == "__main__":
+    from yoho.src.preprocessing.tokenizer import load_tokenizer
+
+    tokenizer = load_tokenizer("./weights/vocab.txt", YOHOConfig())
+    import time
+
+    st = time.monotonic()
+    dataloader = TranscriptionDataloader(
+        YOHOConfig(),
+        tokenizer,
+        4,
+        use_multiprocessing=True,
+        shuffle=True,
+        max_queued_batches=12,
+        num_workers=12,
+    )
+    spectograms, tokens, lengths = dataloader.get_prepared_batch()
+    et = time.monotonic()
+    print(spectograms.shape)
+    print(tokens.shape)
+    print(lengths.shape)
+    print(f"Time to prepare 12 batches: {et-st:.02f}s")
