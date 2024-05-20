0ed1ddaa53004359fb9067864ad490f815aefa3b
JirkaKlimes <jiriklimes920@gmail.com>
2024-05-20 18:09:14 +0200

    refactor

--- yoho/src/config.py
+++ yoho/src/config.py
@@ -2,23 +2,23 @@ from pydantic import BaseModel
 
 
 class YOHOConfig(BaseModel):
-    sample_rate: int = 16000
-    n_mel_bands: int = 128
-    n_fft: int = 400
-    stft_hop: int = 160
+    sample_rate: int
+    n_mel_bands: int
+    n_fft: int
+    stft_hop: int
 
     # Training lengths for the model
     # Exceeding these values reduces accuracy
-    max_audio_len: int = 1024
-    max_text_len: int = 256
+    max_audio_len: int
+    max_text_len: int
 
-    dims: int = 384
+    dims: int
 
-    n_audio_heads: int = 6
-    n_audio_blocks: int = 4
+    n_audio_heads: int
+    n_audio_blocks: int
 
-    n_text_heads: int = 6
-    n_text_blocks: int = 4
+    n_text_heads: int
+    n_text_blocks: int
 
     @property
     def max_input_seconds(self):
