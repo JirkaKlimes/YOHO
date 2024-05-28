318e02f6c73c0b4ed3c8beb907ad67e199b91d45
JirkaKlimes <jiriklimes920@gmail.com>
2024-05-28 23:43:24 +0200

    sample length in seconds

--- train/utils/sample.py
+++ train/utils/sample.py
@@ -13,11 +13,13 @@ class AudioSample:
         sample_rate: int,
         transcript: Optional[str] = None,
         path: Optional[Path] = None,
+        length: Optional[float] = None,
     ):
         self._pcmdata = pcmdata
         self.sample_rate = sample_rate
         self.transcript = transcript
         self._path = path
+        self._length = length
 
     def save(self, path: Path):
         assert path.suffix == ".mp4", "Use `.mp4` since it's the fastest format"
@@ -34,13 +36,24 @@ class AudioSample:
         assert path.suffix == ".mp4", "Can only load `.mp4` files"
         audio_file = MP4(path)
         transcript = audio_file.tags.get("\xa9cmt", [None])[0]
-        return cls(None, sample_rate, transcript, path)
+        length = audio_file.info.length
+        return cls(None, sample_rate, transcript, path, length)
 
     @property
     def pcmdata(self) -> np.ndarray:
         if self._pcmdata is None:
             self._pcmdata = load_audio(path, sample_rate)
         return self._pcmdata
+
+    @property
+    def length(self) -> float:
+        if self._length is None:
+            if self._pcmdata is not None:
+                self._length = len(self._pcmdata) / self.sample_rate
+            else:
+                audio_file = MP4(self._path)
+                self._length = audio_file.info.length
+        return self._length
 
 
 if __name__ == "__main__":
@@ -51,8 +64,12 @@ if __name__ == "__main__":
     transcript = "Hello, World!"
 
     sample = AudioSample(pcm_audio, sample_rate, transcript)
+    print(sample.pcmdata.shape)
+    print(sample.length)
+    print(sample.transcript)
     sample.save(path)
 
     loaded_sample = AudioSample.load(path, sample_rate)
-    print(loaded_sample.transcript)
-    print(loaded_sample.pcmdata)
+    print(sample.pcmdata.shape)
+    print(sample.length)
+    print(sample.transcript)
